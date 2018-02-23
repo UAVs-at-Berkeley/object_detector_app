74960ff829f3f172c499e5e4b5931d3b4debecd4
Raymongdu <raymongdu1998@berkeley.edu>
2018-02-23 01:11:12 -0800

    goto works now, also drone funcs in drone movement now

--- drone_movement.py
+++ drone_movement.py
@@ -1,59 +1,10 @@
 from __future__ import print_function
-import os
-import cv2
-import time
-import argparse
-import multiprocessing
-import numpy as np
-import tensorflow as tf
-
-from utils.app_utils import FPS, WebcamVideoStream
-from multiprocessing import Queue, Pool
-from object_detection.utils import label_map_util
-from object_detection.utils import visualization_utils as vis_util
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""
- Copyright 2015-2016, 3D Robotics.
-guided_set_speed_yaw.py: (Copter Only)
-
-This example shows how to move/direct Copter and send commands in GUIDED mode using DroneKit Python.
-
-Example documentation: http://python.dronekit.io/examples/guided-set-speed-yaw-demo.html
-"""
-
-
 from dronekit import connect, VehicleMode, LocationGlobal, LocationGlobalRelative
 from pymavlink import mavutil # Needed for command message definitions
 import time
 import math
 
-
-#Set up option parsing to get connection string
-import argparse
-parser = argparse.ArgumentParser(description='Control Copter and send commands in GUIDED mode ')
-parser.add_argument('--connect',
-                   help="Vehicle connection target string. If not specified, SITL automatically started and used.")
-args = parser.parse_args()
-
-connection_string = args.connect
-sitl = None
-
-
-#Start SITL if no connection string specified
-if not connection_string:
-    import dronekit_sitl
-    sitl = dronekit_sitl.start_default()
-    connection_string = sitl.connection_string()
-
-
-# Connect to the Vehicle
-print('Connecting to vehicle on: %s' % connection_string)
-vehicle = connect(connection_string, wait_ready=True)
-print("type: ", str(type(vehicle)))
-
-def arm_and_takeoff(aTargetAltitude):
+def arm_and_takeoff(vehicle, aTargetAltitude):
     """
     Arms vehicle and fly to aTargetAltitude.
     """
@@ -86,7 +37,7 @@ def arm_and_takeoff(aTargetAltitude):
             break
         time.sleep(1)
 
-def goto(dNorth, dEast, gotoFunction=vehicle.simple_goto):
+def goto(vehicle, dNorth, dEast, gotoFunction=None):
     """
     Moves the vehicle to a position dNorth metres North and dEast metres East of the current position.
 
@@ -101,20 +52,23 @@ def goto(dNorth, dEast, gotoFunction=vehicle.simple_goto):
     targetLocation = get_location_metres(currentLocation, dNorth, dEast)
     targetDistance = get_distance_metres(currentLocation, targetLocation)
     #gotoFunction(targetLocation)
-    vehicle.simple_goto(targetLocation)
-    print("location", currentLocation)
-    print("location", targetLocation)
+    print("Initial distance: ", targetDistance)
+    gotoFunction(targetLocation)
+    # print("location", currentLocation)
+    # print("location", targetLocation)
     #print "DEBUG: targetLocation: %s" % targetLocation
     #print "DEBUG: targetLocation: %s" % targetDistance
 
-    while vehicle.mode.name=="GUIDED": #Stop action if we are no longer in guided mode.
-        #print "DEBUG: mode: %s" % vehicle.mode.name
-        remainingDistance=get_distance_metres(vehicle.location.global_relative_frame, targetLocation)
-        print("Distance to target: ", remainingDistance)
-        if remainingDistance<=targetDistance*0.01: #Just below target, in case of undershoot.
-            print("Reached target")
-            break;
-        time.sleep(2)
+    # while vehicle.mode.name=="GUIDED": #Stop action if we are no longer in guided mode.
+    #     #print "DEBUG: mode: %s" % vehicle.mode.name
+    #     remainingDistance=get_distance_metres(vehicle.location.global_relative_frame, targetLocation)
+    #     print("Distance to target: ", remainingDistance)
+    #     if remainingDistance<=targetDistance*0.01: #Just below target, in case of undershoot.
+    #         print("Reached target")
+    #         break;
+    #     time.sleep(2)
+    time.sleep(2)
+    print("distance after move: ", get_distance_metres(vehicle.location.global_relative_frame, targetLocation))
 def get_location_metres(original_location, dNorth, dEast):
     """
     Returns a LocationGlobal object containing the latitude/longitude `dNorth` and `dEast` metres from the
@@ -174,9 +128,3 @@ def get_bearing(aLocation1, aLocation2):
     if bearing < 0:
         bearing += 360.00
     return bearing;
-def go():
-    arm_and_takeoff(5)
-    print("Set groundspeed to 5m/s.")
-    vehicle.groundspeed=5
-    goto(80, -50)
-go()
--- object_detection_app.py
+++ object_detection_app.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 import os
 import cv2
 import time
@@ -6,14 +5,12 @@ import argparse
 import multiprocessing
 import numpy as np
 import tensorflow as tf
-#import drone_movement
 
 from utils.app_utils import FPS, WebcamVideoStream
 from multiprocessing import Queue, Pool
 from object_detection.utils import label_map_util
 from object_detection.utils import visualization_utils as vis_util
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+import drone_movement
 
 """
  Copyright 2015-2016, 3D Robotics.
@@ -53,143 +50,9 @@ if not connection_string:
 print('Connecting to vehicle on: %s' % connection_string)
 vehicle = connect(connection_string, wait_ready=True)
 print("type: ", str(type(vehicle)))
-string = "hello"
-d = {"s": string, "v": vehicle}
-def arm_and_takeoff(aTargetAltitude):
-    """
-    Arms vehicle and fly to aTargetAltitude.
-    """
-    print("Basic pre-arm checks")
-    # Don't let the user try to arm until autopilot is ready
-    while not vehicle.is_armable:
-        print(" Waiting for vehicle to initialise...")
-        time.sleep(1)
 
-
-    print("Arming motors")
-    # Copter should arm in GUIDED mode
-    vehicle.mode = VehicleMode("GUIDED")
-    vehicle.armed = True
-
-    while not vehicle.armed:
-        print(" Waiting for arming...")
-        time.sleep(1)
-
-    print("Taking off!")
-    vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
-
-    # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
-    #  after Vehicle.simple_takeoff will execute immediately).
-    while True:
-        print(" Altitude: ", vehicle.location.global_relative_frame.alt)
-        if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95: #Trigger just below target alt.
-            print("Reached target altitude")
-            break
-        time.sleep(1)
-
-def goto(dNorth, dEast, gotoFunction=vehicle.simple_goto):
-    """
-    Moves the vehicle to a position dNorth metres North and dEast metres East of the current position.
-
-    The method takes a function pointer argument with a single `dronekit.lib.LocationGlobal` parameter for
-    the target position. This allows it to be called with different position-setting commands.
-    By default it uses the standard method: dronekit.lib.Vehicle.simple_goto().
-
-    The method reports the distance to target every two seconds.
-    """
-    currentLocation = d["v"].location.global_relative_frame
-    targetLocation = get_location_metres(currentLocation, dNorth, dEast)
-    targetDistance = get_distance_metres(currentLocation, targetLocation)
-    d["v"].simple_goto(targetLocation)
-    print("location", currentLocation)
-    print("location", targetLocation)
-    #print "DEBUG: targetLocation: %s" % targetLocation
-    #print "DEBUG: targetLocation: %s" % targetDistance
-
-    while d["v"].mode.name=="GUIDED": #Stop action if we are no longer in guided mode.
-        #print "DEBUG: mode: %s" % vehicle.mode.name
-        remainingDistance=get_distance_metres(d["v"].location.global_relative_frame, targetLocation)
-        print("Distance to target: ", remainingDistance)
-        d["v"].groundspeed += 1
-        d["s"] += " ."
-        print("test", d["s"])
-        print("spped", d["v"].groundspeed)
-        if remainingDistance<=targetDistance*0.01: #Just below target, in case of undershoot.
-            print("Reached target")
-            break;
-        time.sleep(2)
-def get_location_metres(original_location, dNorth, dEast):
-    """
-    Returns a LocationGlobal object containing the latitude/longitude `dNorth` and `dEast` metres from the
-    specified `original_location`. The returned LocationGlobal has the same `alt` value
-    as `original_location`.
-
-    The function is useful when you want to move the vehicle around specifying locations relative to
-    the current vehicle position.
-
-    The algorithm is relatively accurate over small distances (10m within 1km) except close to the poles.
-
-    For more information see:
-    http://gis.stackexchange.com/questions/2951/algorithm-for-offsetting-a-latitude-longitude-by-some-amount-of-meters
-    """
-    earth_radius = 6378137.0 #Radius of "spherical" earth
-    #Coordinate offsets in radians
-    dLat = dNorth/earth_radius
-    dLon = dEast/(earth_radius*math.cos(math.pi*original_location.lat/180))
-
-    #New position in decimal degrees
-    newlat = original_location.lat + (dLat * 180/math.pi)
-    newlon = original_location.lon + (dLon * 180/math.pi)
-    if type(original_location) is LocationGlobal:
-        targetlocation=LocationGlobal(newlat, newlon,original_location.alt)
-    elif type(original_location) is LocationGlobalRelative:
-        targetlocation=LocationGlobalRelative(newlat, newlon,original_location.alt)
-    else:
-        raise Exception("Invalid Location object passed")
-
-    return targetlocation;
-
-
-def get_distance_metres(aLocation1, aLocation2):
-    """
-    Returns the ground distance in metres between two LocationGlobal objects.
-
-    This method is an approximation, and will not be accurate over large distances and close to the
-    earth's poles. It comes from the ArduPilot test code:
-    https://github.com/diydrones/ardupilot/blob/master/Tools/autotest/common.py
-    """
-    dlat = aLocation2.lat - aLocation1.lat
-    dlong = aLocation2.lon - aLocation1.lon
-    return math.sqrt((dlat*dlat) + (dlong*dlong)) * 1.113195e5
-
-
-def get_bearing(aLocation1, aLocation2):
-    """
-    Returns the bearing between the two LocationGlobal objects passed as parameters.
-
-    This method is an approximation, and may not be accurate over large distances and close to the
-    earth's poles. It comes from the ArduPilot test code:
-    https://github.com/diydrones/ardupilot/blob/master/Tools/autotest/common.py
-    """
-    off_x = aLocation2.lon - aLocation1.lon
-    off_y = aLocation2.lat - aLocation1.lat
-    bearing = 90.00 + math.atan2(-off_y, off_x) * 57.2957795
-    if bearing < 0:
-        bearing += 360.00
-    return bearing;
-
-def go():
-    arm_and_takeoff(5)
-    print("Set groundspeed to 5m/s.")
-    vehicle.groundspeed=5
-    goto(80, -50)
-
-
-#Arm and take of to altitude of 5 meters
-arm_and_takeoff(5)
+drone_movement.arm_and_takeoff(vehicle, 5)
 vehicle.groundspeed=5
-# goto(80, -50) #this works
-# go(80, -50) #so does this
 
 CWD_PATH = os.getcwd()
 
@@ -207,8 +70,10 @@ label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES,
                                                             use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
-
-
+box_x = 0;
+box_y = 0;
+def get_center():
+    return box_x, box_y
 def detect_objects(image_np, sess, detection_graph):
     # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
     image_np_expanded = np.expand_dims(image_np, axis=0)
@@ -239,16 +104,9 @@ def detect_objects(image_np, sess, detection_graph):
     scores = scores[0,i:i+1]
     scores = np.reshape(scores, (1))
     print(boxes[0])
-    ymin, xmin, ymax, xmax = boxes[0][0], boxes[0][1], boxes[0][2], boxes[0][3]
-    ycenter = (ymin + ymax)/2
-    xcenter = (xmin + xmax)/2
-    # print(ycenter, xcenter)
-    # arm_and_takeoff(5)
-    #
-    # print("Set groundspeed to 5m/s.")
-    vehicle.groundspeed=5
-    goto(ycenter*100, xcenter*100) #doesn't work in here
-    #go()
+    x = (boxes[0][1]+ boxes[0][3])/2
+    y = (boxes[0][0]+ boxes[0][2])/2
+    output_q.put((x, y))
     # moveDrone(boxes[0])
     num_detections = 1
 
@@ -282,22 +140,7 @@ def worker(input_q, output_q):
         frame = input_q.get()
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        # currentLocation = vehicle.location.global_relative_frame
-        # targetLocation = get_location_metres(currentLocation, 80, -50)
-        # targetDistance = get_distance_metres(currentLocation, targetLocation)
-        # print("DIST: ", targetDistance)
-        # vehicle.groundspeed=5
-        # d["v"].simple_goto(targetLocation)
-        # while vehicle.mode.name=="GUIDED": #Stop action if we are no longer in guided mode.
-        #     #print "DEBUG: mode: %s" % vehicle.mode.name
-        #     remainingDistance=get_distance_metres(vehicle.location.global_relative_frame, targetLocation)
-        #     print("Distance to target: ", remainingDistance)
-        #     if remainingDistance<=targetDistance*0.01: #Just below target, in case of undershoot.
-        #         print("Reached target")
-        #         break;
-        #     time.sleep(2)
         output_q.put(detect_objects(frame_rgb, sess, detection_graph))
-
     fps.stop()
     sess.close()
 
@@ -321,9 +164,8 @@ if __name__ == '__main__':
 
     input_q = Queue(maxsize=args.queue_size)
     output_q = Queue(maxsize=args.queue_size)
-    vehicle.groundspeed=5
-    print("HELLO??", vehicle.groundspeed)
-    pool = Pool(1, worker, (input_q, output_q))
+    pool = Pool(args.num_workers, worker, (input_q, output_q))
+
     video_capture = WebcamVideoStream(src=args.video_source,
                                       width=args.width,
                                       height=args.height).start()
@@ -334,12 +176,18 @@ if __name__ == '__main__':
         input_q.put(frame)
 
         t = time.time()
-
+        box_x,box_y = output_q.get()
+        scaling_factor = 10
+        offset_x = (box_x - 0.5)*scaling_factor
+        offset_y = (box_y - 0.5)*scaling_factor
+        drone_movement.goto(vehicle, offset_y, offset_x, vehicle.simple_goto)
+        #print("OUTPUT", box_x, " ", box_y)
         output_rgb = cv2.cvtColor(output_q.get(), cv2.COLOR_RGB2BGR)
         cv2.imshow('Video', output_rgb)
         fps.update()
 
         print('[INFO] elapsed time: {:.2f}'.format(time.time() - t))
+        #print(get_center())
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -348,6 +196,6 @@ if __name__ == '__main__':
     print('[INFO] elapsed time (total): {:.2f}'.format(fps.elapsed()))
     print('[INFO] approx. FPS: {:.2f}'.format(fps.fps()))
 
-    #pool.terminate()
+    pool.terminate()
     video_capture.stop()
     cv2.destroyAllWindows()
